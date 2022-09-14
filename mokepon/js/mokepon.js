4c06c20cf6ac8f200ad10c16c89a8b8a459e4e99
Lucia Gonzalez <lucialezmor@gmail.com>
2022-09-14 01:59:31 -0300

    Creacion de class Mokepon y objetos (personajes) a partir de esa clase

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -29,6 +29,34 @@ let ataqueEnemigo;
 let vidasJugador = 3;
 let vidasEnemigo = 3;
 
+// clase, va con mayuscula, es el "plano" para el objeto (el personaje en este caso). Es escalable, se le pueden agregar mas propiedades
+class Mokepon {
+    constructor(nombre, foto, vida) {
+        this.nombre = nombre;
+        this.foto = foto;
+        this.vida = vida;
+    }
+}
+
+//objeto hecho con la clase Mokepon. los valores con comillas simples
+let hipodoge = new Mokepon(
+    "Hipodoge",
+    "assets/mokepons_mokepon_hipodoge_attack.png",
+    5
+);
+let capipepo = new Mokepon(
+    "Capipepo",
+    "assets/mokepons_mokepon_capipepo_attack.png",
+    5
+);
+let ratigueya = new Mokepon(
+    "Ratigueya",
+    "assets/mokepons_mokepon_ratigueya_attack.png",
+    5
+);
+//para probar objeto
+//console.log(hipodoge);
+
 function iniciarJuego() {
     seccionSeleccionarAtaque.style.display = "none";
     seccionReiniciar.style.display = "none";
