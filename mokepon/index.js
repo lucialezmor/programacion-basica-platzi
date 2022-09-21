d69a1508238c8e016fb8ab0cf07f3c0c69cbe2e4
Lucia Gonzalez <lucialezmor@gmail.com>
2022-09-21 01:16:10 -0300

    Hola mundo con express y node!

---
+++ mokepon/index.js
@@ -0,0 +1,28 @@
+// console.log("hola node");
+
+/*3 cosas que con lo minimo necesario par autilizar express*/
+
+//a) importar la libreria - con requiere podemos usar lo que instalamos con npm. ya esta importada destro de mi proyecto
+// puedo crear una aplicacion que vaa representar a nuestro servidor
+const express = require("express");
+
+//b)almacena nuestra aplicacion. instancia del servidor que voy a estar utilizando
+const app = express();
+
+//url, metodo
+app.get("/", (req, res) => {
+    res.send("hola");
+});
+
+//que escuche las peticiones de nuestros clientes por medio de un puerto. listen nos permite agregar la capacidad de iniciar el servidor
+//c)necesita minimo un callback y el puerto que vamos a utilizar para que sirva la respuesta
+app.listen(8080, () => {
+    console.log("el servidor funcionando");
+});
+/* fin de las minimas 3 cosas*/
+/*ctrl c para apagar el servidor*/
+
+//url, metodo
+app.get("/", (req, res) => {
+    res.send("hola");
+});
