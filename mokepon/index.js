b6d7a1d0a83a82fdc045e44528574a946681bc67
Lucia Gonzalez <lucialezmor@gmail.com>
2022-09-27 01:03:17 -0300

    transmision de coordenadas via post

--- mokepon/index.js
+++ mokepon/index.js
@@ -28,6 +28,11 @@ class Jugador {
     asignarMokepon(mokepon) {
         this.mokepon = mokepon;
     }
+
+    actualizarPosicion(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 class Mokepon {
@@ -71,6 +76,23 @@ app.post("/mokepon/:jugadorId", (req, res) => {
 
     console.log(jugadores);
     console.log(jugadorId);
+    res.end();
+});
+
+//actualizar la posicion del jugador en el mapa
+app.post("/mokepon/:jugadorId/posicion", (req, res) => {
+    const jugadorId = req.params.jugadorId || "";
+    const x = req.body.x || 0;
+    const y = req.body.y || 0;
+
+    const jugadorIndex = jugadores.findIndex(
+        (jugador) => jugadorId === jugador.id
+    );
+
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].actualizarPosicion(x, y);
+    }
+
     res.end();
 });
 
--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -431,6 +431,9 @@ function pintarCanvas() {
     lienzo.clearRect(0, 0, mapa.width, mapa.height);
     lienzo.drawImage(mapaBackground, 0, 0, mapa.width, mapa.height);
     mascotaJugadorObjeto.pintarMokepon();
+
+    enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y);
+
     hipodogeEnemigo.pintarMokepon();
     capipepoEnemigo.pintarMokepon();
     ratigueyaEnemigo.pintarMokepon();
@@ -443,6 +446,19 @@ function pintarCanvas() {
         revisarColision(capipepoEnemigo);
         revisarColision(ratigueyaEnemigo);
     }
+}
+
+function enviarPosicion(x, y) {
+    fetch(`http://localhost:8080//mokepon/${jugadorId}/posicion`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            x,
+            y,
+        }),
+    });
 }
 
 function moveUp() {
